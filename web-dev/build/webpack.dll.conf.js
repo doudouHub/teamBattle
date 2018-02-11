e162b343dcb2922098e72fc950c758e6d2113f4e
doudouHub <1287564321@qq.com>
2018-02-11 16:58:09 +0800

    互动对战-目录更新，更新webpack配置

--- web-dev/build/webpack.dll.conf.js
+++ web-dev/build/webpack.dll.conf.js
@@ -1,29 +1,61 @@
-const path = require('path');
-const webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: {
-    vendor: ['vue/dist/vue.common.js','vue-router', 'babel-polyfill','axios']
-  },
-  output: {
-    path: path.join(__dirname, '../static/js'),
-    filename: '[name].dll.js',
-    library: '[name]_library'
-  },
-  plugins: [
-    new webpack.DllPlugin({
-      path: path.join(__dirname, '.', '[name]-manifest.json'),
-      name: '[name]_library'
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ]
+    entry: {
+        vendor: [
+            'vue/dist/vue.common.js',
+            'vue-router',
+            'babel-polyfill',
+            'axios',
+            'vuex'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, '../static/js'),
+        filename: '[name].dll.js',
+        library: '[name]_library'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        // new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn|en-gb/),
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '.', '[name]-manifest.json'),
+            libraryTarget: 'commonjs2',
+            name: '[name]_library'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '.', '[name]-manifest.json'),
+            name: '[name]_library'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ]
 };
