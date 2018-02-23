d895e86faaf501caefb7e0e03df639aabe8a9e5d
doudouHub <1287564321@qq.com>
2018-02-23 10:33:17 +0800

    互动对战-目录更新，更新webpack配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
         parser: 'babel-eslint'
     },
     env: {
+        "node":true,
         browser: true,
     },
     extends: [
@@ -19,16 +20,21 @@ module.exports = {
     plugins: [
         'vue'
     ],
-    // add your custom rules here
+    // add your custom rules here 0 不提示，1 警告，2 抛出错误
     rules: {
-        // allow async-await
+        // 生成器函数*的前后空格
         'generator-star-spacing': 'off',
-        // allow debugger during development
+        // 禁止使用debugger
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         // 空格2个
         'indent': [0, 4, {'SwitchCase': 1}],
+        // 引号类型 `` "" ''
         "quotes": [0, "single"],
         // if while function 后面的{必须与if在同一行，java风格。
         'brace-style': [0, '1tbs', {'allowSingleLine': true}],
+        // 语句强制分号结尾
+        "semi": [0, "always"],
+        // 连续声明
+        "one-var": 0,
     }
 }
